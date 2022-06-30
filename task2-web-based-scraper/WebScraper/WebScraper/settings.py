f8a2d44d1848d34fa60d6cda947d57582c43affc
NamanGokharu <namangokharu01@gmail.com>
2022-06-30 13:32:14 +0530

    Secret key config

--- task2-web-based-scraper/WebScraper/WebScraper/settings.py
+++ task2-web-based-scraper/WebScraper/WebScraper/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gh=3(l*bs7&o)f@1yc0!i$&g=07$bwbld3ui53%d#)7m35flx9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
